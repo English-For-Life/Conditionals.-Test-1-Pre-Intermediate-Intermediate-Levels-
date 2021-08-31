4d60b0914aa064bc41844423cc5a265857f20951
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-31 11:58:10 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"I’m small. If only I _____ taller.",
+        options:["were", "wasn't", "am", "have"],
+        answer: 0
+    },
+    {
+        q:"'We have mice in the kitchen.' - 'If you _____ a cat, the mice _____ soon _____ .'",
+        options:["have / will / disappear", "had / would / disappear", "are / will / disappear", "had / will / disappear"],
+        answer: 1
+    },
+    {
+        q:"We live in the city. We wish we _____ in the country.",
+        options:["live", "living", "lived", "have lived"],
+        answer: 2
+    },
+    {
+        q:"We live in the city. If only we _____ in the city!",
+        options:["live", "living", "haven't lived", "didn't live"],
+        answer: 3
+    },
+    {
+        q:"I’m not having a holiday this year. I wish I _____ a holiday.",
+        options:["was having", "am having", "have", "will have"],
+        answer: 0
+    },
+    {
+        q:"I’m going to the dentist tomorrow. I wish I _____ to the dentist.",
+        options:["am not going", "wasn’t going", "went", "will go"],
+        answer: 1
+    },
+    {
+        q:"I can’t ski. I wish I _____ ski.",
+        options:["will", "can", "could", "couldn't"],
+        answer: 2
+    },
+    {
+        q:"I wasn’t happy at school. I wish I _____ happy.",
+        options:["had had", "has been", "will have", "had been"],
+        answer: 3
+    },
+    {
+        q:"He didn’t pass his driving test. He wishes he _____ it.",
+        options:["had passed", "have passed", "will pass", "pass"],
+        answer: 0
+    },
+    {
+        q:"I haven’t been to Beijing. I wish I _____ to Beijing.",
+        options:["have been", "had been", "has been", "will be"],
+        answer: 1
+    }
+
+
+
+]
